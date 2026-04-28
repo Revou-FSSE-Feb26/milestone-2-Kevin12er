58d03a386824eb0574f1d75584b3442eb6cf94b4
Kevin Langga <kikoirfan45@gmail.com>
2026-04-29 04:29:09 +0700

    explanation in gameketik.js and tebakangka.js

--- Javascript/game-ketik.js
+++ Javascript/game-ketik.js
@@ -12,53 +12,53 @@
     let intervalTime; /*variabel interval time disini berfungsi */
  
     function tampilKata(max) {
-        const acakKata = Math.floor(Math.random() * max.length);
-        if (max[acakKata] === textDisplay.textContent.toLowerCase()) {
-            tampilKata(max);
+        const acakKata = Math.floor(Math.random() * max.length); /*ini untuk pembulatan angka kebawah dan memilih kata di array secara acak*/
+        if (max[acakKata] === textDisplay.textContent.toLowerCase()) { /*kenapa to lower case supaya pembandingnnya semua dikonversi menjadi huruf kecil walaupun user ngetik pakai huruf besar*/
+            tampilKata(max); /*fungsi yang memanggil dirinya sendiri*/
         } else {
             textDisplay.textContent = max[acakKata];
         }
     }
  
     papanKetik.addEventListener("keypress", function (e) {
-        if (e.key === "Enter") {
-            if (papanKetik.value.toLowerCase() === textDisplay.textContent.toLowerCase()) {
-                textDisplay.style.color = "limegreen";
-                papanKetik.value = "";
-                skor++;
+        if (e.key === "Enter") { /*supaya si user bisa tekan enter aja sih ini, keypress kan */
+            if (papanKetik.value.toLowerCase() === textDisplay.textContent.toLowerCase()) { /*supaya pembandingnya di konversi menajdi huruf kecil semua*/
+                textDisplay.style.color = "limegreen"; /*jika si user berhasil mengetik kata tersebut dan enter ? maka warnya hijau*/
+                papanKetik.value = ""; /*display langsung diskosongkan*/
+                skor++; /*dan updatee lah skornya*/
                 setTimeout(function () {
                     textDisplay.style.color = "";
                     tampilKata(kata);
-                }, 1000);
+                }, 1000); /*set time out diatas ini fungsinya untuk beri jeda selama 1 detik sebelum kata di update lagi*/
             } else {
-                textDisplay.style.color = "red";
-                papanKetik.value = "";
-                setTimeout(function () {
-                    textDisplay.style.color = "";
+                textDisplay.style.color = "red"; /*warna merah jika gagal*/
+                papanKetik.value = ""; /*kosongkan display tepat setelah enter*/
+                setTimeout(function () { 
+                    textDisplay.style.color = ""; 
                     tampilKata(kata);
-                }, 1000);
+                }, 1000); /*hal yang saya saya terapkan ulang jadi mau benar atau salah, kasih waktu jeda selama 1 detik*/
             }
  
-            skorKetik.textContent = `skor: ${skor}`;
+            skorKetik.textContent = `skor: ${skor}`; /*Update skor di UI ada berapa kata yang berhasil diketik beliau selama di game*/
         }
     });
  
     function mulaiTimer() {
-        clearInterval(intervalTime);
-        intervalTime = setInterval(() => {
-            nilaiTimer--;
-            const menit = Math.floor(nilaiTimer / 60);
+        clearInterval(intervalTime); /*mencegah penumpukan interval*/
+        intervalTime = setInterval(() => { /*di line ini game mulai dijalankan*/
+            nilaiTimer--; /*nilai timer akan dikurangi 1 detik setelah game dijalankan*/
+            const menit = Math.floor(nilaiTimer / 60); /*karena divariabel global saya set 120, saya harus bagi 60 untuk nanti mau saya ubah model timernya jad 02:00*/
             const detik = nilaiTimer % 60;
  
             secondCount.textContent =
-                String(menit).padStart(2, "0") + ":" + String(detik).padStart(2, "0");
+                String(menit).padStart(2, "0") + ":" + String(detik).padStart(2, "0"); /*disini pengkonversiannya*/
  
-            if (nilaiTimer === 0) {
-                clearInterval(intervalTime);
-                btnResetWord.textContent = "Start";
-                nilaiTimer = 120;
-                textDisplay.textContent = `Game Selesai skor kamu: ${skor}`;
-                setTimeout(function () {
+            if (nilaiTimer === 0) { /*titik dimana nilai timer habis*/
+                clearInterval(intervalTime); /*disini harus clear interval karena kalau tidak dia akan terus berjalan jadi minus*/
+                btnResetWord.textContent = "Start"; /*ketika game berjalan kan tulisan tombolnya jadi reset tuh, nah kalau waktu habis tombolnya balik tulisan star*/
+                nilaiTimer = 120; /*waktu balik menjadi 2 menit */
+                textDisplay.textContent = `Game Selesai skor kamu: ${skor}`; /*update skor di UI*/
+                setTimeout(function () { /*ini adalh waktu berapa lama text game selesai di display tampil di UI dan diganti sama text display dibawah*/
                     if (skor >= 45) {
                         textDisplay.textContent = `leh uga nih cepet banget`;
                     } else if (skor >= 30) {
@@ -73,19 +73,22 @@
         }, 1000);
     }
  
+
+
+    /*dibawah ini kita mengembalikan semua nilai kedefault(awal)*/
     btnResetWord.addEventListener("click", function () {
         if (btnResetWord.textContent === "Start") {
             mulaiTimer();
             tampilKata(kata);
             btnResetWord.textContent = `Reset`;
         } else {
-            clearInterval(intervalTime);
-            textDisplay.textContent = "";
-            nilaiTimer = 120;
-            skor = 0;
-            skorKetik.textContent = `skor: ${skor}`;
-            secondCount.textContent = `02:00`;
-            btnResetWord.textContent = `Start`;
+            clearInterval(intervalTime); /*ini adalah logic ketika tombol reset ditekan maka clear interval atau stop waktunya*/
+            textDisplay.textContent = ""; /*kosongkan displaynya*/
+            nilaiTimer = 120; /*balikin waktunya keawal*/
+            skor = 0; /*reset skornya jadi 0*/
+            skorKetik.textContent = `skor: ${skor}`; /*update skornya di ui jadi 0 lagi*/
+            secondCount.textContent = `02:00`; /*balikin waktunya ke 2 menit */
+            btnResetWord.textContent = `Start`; /*ganti tulisan buttonnya jadi start*/
         }
     });
 })();
--- Javascript/rock-paper.js
+++ Javascript/rock-paper.js
@@ -1,4 +1,4 @@
-(function () {
+(function () { /*aku pakai IIFE untuk gabungin semua kodingan*/
     const skorSuit = document.getElementById("skor-suit");
     const pilihanUser = document.getElementById("pilihan-user");
     const pilihanKomputer = document.getElementById("pilihan-komputer");
@@ -11,71 +11,75 @@
     const nyawaUser = document.getElementById("nyawa-user");
     const opsiKomputer = ["batu", "gunting", "kertas"];
  
-    let skorKomputer = 0;
-    let skorTerbaru = 0;
-    let seriTerbaru = 0;
-    let nyawamu = 7;
+    let skorKomputer = 0; /*variabel penampung skor komputer*/
+    let skorTerbaru = 0; /*variabel penampung skor User*/
+    let seriTerbaru = 0; /*variabel penampung skor seri*/
+    let nyawamu = 7; /*variabel penampung untuk nyawa*/
  
     daftarTombol.forEach((tombol) => {
         tombol.addEventListener("click", function () {
-            if (nyawamu === 0) return;
+            if (nyawamu === 0) return; /*jika nyawa 0 maka return hasilnya siapa yang menang*/
  
             const userChoice = this.dataset.pilihan;
             console.log(`kamu memilih: ${userChoice}`);
  
-            const opsiAcak = Math.floor(Math.random() * opsiKomputer.length);
+            const opsiAcak = Math.floor(Math.random() * opsiKomputer.length); /*supaya komputer memilihnya acak cuma gatau kenapa komputernya menang terus bjir*/
             const compChoice = opsiKomputer[opsiAcak];
  
-            pilihanUser.textContent = userChoice;
-            pilihanKomputer.textContent = compChoice;
+            pilihanUser.textContent = userChoice; /*nilai yang diambil oleh user maka tampilkan di display*/
+            pilihanKomputer.textContent = compChoice; /*nilai yang diambil oleh komputer maka tampilkan didisplay*/
  
             setTimeout(function () {
-                if (userChoice === compChoice) {
-                    seriTerbaru++;
-                    seriSkor.textContent = seriTerbaru;
-                } else if (
+                if (userChoice === compChoice) { /*ini logic untuk hasil seri*/
+                    seriTerbaru++;  /*tambahkan nilai ke variabel seri*/
+                    seriSkor.textContent = seriTerbaru; /*lalu update skornya ke UI*/
+                } else if ( /*logic untuk kemenangan user*/
                     (userChoice === "batu" && compChoice === "gunting") ||
                     (userChoice === "gunting" && compChoice === "kertas") ||
                     (userChoice === "kertas" && compChoice === "batu")
                 ) {
-                    skorTerbaru++;
-                    angkaSkor.textContent = skorTerbaru;
+                    skorTerbaru++; /*update skor dulu*/
+                    angkaSkor.textContent = skorTerbaru; /*baru tampilkan di display*/
                 } else {
-                    skorKomputer++;
-                    angkaKomputer.textContent = skorKomputer;
+                    skorKomputer++; /*pengecualian dari kedua kemungkinan diatas adalah komputer menang maka tambahkan skornya*/
+                    angkaKomputer.textContent = skorKomputer; /*baru update skornya ke display*/
                 }
  
-                nyawamu--;
-                nyawaUser.textContent = nyawamu;
+                nyawamu--; /*Pengurangan nyawa dengan "--" kalau "++" malah nambah pak dia nyawanya */
+                nyawaUser.textContent = nyawamu; /*update nyawa terbaru karena sequencenya nilai diatas (nyawamu--) baru tampilkan*/
  
                 if (nyawamu <= 0) {
                     if (skorTerbaru > skorKomputer) {
-                        displayPemenang.textContent = `Selamat, kamu memenangkan game`;
-                        displayPemenang.style.color = "green";
+                        displayPemenang.textContent = `Selamat, kamu memenangkan game`; /*display UI ketika user memenangkan game*/
+                        displayPemenang.style.color = "green"; /*warnya hijau kalau menang*/
                     } else if (skorTerbaru < skorKomputer) {
-                        displayPemenang.textContent = `kamu kalah, computer menang`;
-                        displayPemenang.style.color = "red";
+                        displayPemenang.textContent = `kamu kalah, computer menang`; /*display UI ketika computer memenangkan game*/
+                        displayPemenang.style.color = "red"; /*warna merah karena user kalah*/
                     } else {
-                        displayPemenang.textContent = `Hasil Seri`;
-                        displayPemenang.style.color = "";
+                        displayPemenang.textContent = `Hasil Seri`; /*display UI kalau hasilnya seri*/
+                        displayPemenang.style.color = ""; /*default dispaly UI*/
                     }
                 }
-            }, 100);
+            }, 100); /*ini adalah set time out yang saya setting hanya 100ms setelah game selesai untuk melihat display diatas*/
         });
     });
  
+
+
+
+    // dibawah ini adalah bagaiman saya mereset semua kembali ke titik awal permainan... mengembalikan semua nilai seperti semula 
     btnResetSuit.addEventListener("click", function () {
-        nyawamu = 7;
-        nyawaUser.textContent = nyawamu;
-        skorKomputer = 0;
-        angkaKomputer.textContent = "0";
-        skorTerbaru = 0;
-        angkaSkor.textContent = "0";
-        seriTerbaru = 0;
-        seriSkor.textContent = "0";
-        displayPemenang.textContent = "Batu Gunting Kertas";
-        displayPemenang.style.color = "orange";
-        pilihanUser.textContent = "?";
-        pilihanKomputer.textContent = "?";
+        nyawamu = 7;    /*nyawa kembali menjadi 7 / awalnya 7 memang bang*/
+        nyawaUser.textContent = nyawamu; /*menampilkan display nyawa: 7 di ui (frontend)*/
+        skorKomputer = 0; /*skor komputer balik 0 lagi bang*/
+        angkaKomputer.textContent = "0"; /*tampilan display ui skor komputer jadi 0 lagi*/
+        skorTerbaru = 0; /*reset skor*/
+        angkaSkor.textContent = "0"; /*tampilkan reset skor di ui*/
+        seriTerbaru = 0; /*skor seri jadi 0*/
+        seriSkor.textContent = "0"; /*tampilkan skor seri di UI*/
+        displayPemenang.textContent = "Batu Gunting Kertas"; /*nilai awal array*/
+        displayPemenang.style.color = "orange"; /* warna default styling*/
+        pilihanUser.textContent = "?"; /*simbol default ketika game tidak dimulai di UI*/
+        pilihanKomputer.textContent = "?"; /*simbol default ketika game tidak dimulai di UI*/
     });
 })();
